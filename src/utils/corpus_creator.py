d29e7f57613b031327b17c8e5ba9295b14117a1a
zainl <lukas.zain98@gmail.com>
2024-02-17 15:48:48 +0100

    corpus creator with reservoir

--- src/utils/corpus_creator.py
+++ src/utils/corpus_creator.py
@@ -1,20 +1,32 @@
 from torch.utils.data import DataLoader
 import torch
+import random
 
-def make_corpus(corpus_loader: DataLoader, corpus_size=1000):
+def make_corpus(corpus_loader: DataLoader, corpus_size=100, n_classes=2):
     """
-    incrementally samples from corpus loader, keeps class balance
+    incrementally samples from corpus loader, keeps class balance, performs reservoir sampling to sample with equal likelihood for every file
     """
-    corpus_data = [torch.tensor(),torch.tensor()]
+    corpus_files = []
+    labels = []
+    labelk = [0]*n_classes
 
     for data, label in corpus_loader:# do reservoir sampling
         for file, label in zip(data, label):
-            if(len(corpus_data)>=corpus_size):
-                pass
-            else:
-                corpus_data[0].append(file)
-                corpus_data[1].append(label)
-        
+            if(len(corpus_files)>=corpus_size):# if corpus is full replace random element with probability (corpus_size/2)/labelk[label]
+                corpus_1_indices = [i for i,x in enumerate(labels) if x == label]
+                labelk[label]+=1
+                sample_index = random.choice(range(labelk[label]))
+                if sample_index < len(corpus_1_indices):
+                    corpus_files[corpus_1_indices[sample_index]]=file
+                    labels[corpus_1_indices[sample_index]]=label
+            else:     # fill corpus with files, keep class balance
+                if labels.count(label)<corpus_size//n_classes:
+                    corpus_files.append(file)
+                    labels.append(label)
+                    labelk[label]+=1
 
-    return corpus_data
+    corpus_files = torch.stack(corpus_files)    
+    labels = torch.stack(labels)
+
+    return [corpus_files, labels]
 
