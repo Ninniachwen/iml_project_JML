51b1b854c1f32e0532f64be3145d79d00a22ef68
zainl <lukas.zain98@gmail.com>
2024-02-18 23:42:07 +0100

    train_or_load_new_datset_model v1

--- src/classifier_versions.py
+++ src/classifier_versions.py
@@ -2,6 +2,7 @@ import inspect
 import os
 import sys
 import torch
+from torch.utils.data import DataLoader
 
 # access model in parent dir: https://stackoverflow.com/a/11158224/14934164
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -13,6 +14,12 @@ sys.path.insert(0, "")  # noqa
 
 from Original_Code.src.simplexai.models.image_recognition import MnistClassifier
 from Original_Code.src.simplexai.experiments import mnist
+from src.cats_and_dogs_training import train_model, load
+from src.models.CatsAndDogsModel import CatsAndDogsClassifier
+from src.cats_and_dogs_predictions import load_model
+from src.datasets.cats_and_dogs_dataset import CandDDataSet
+from src.utils.image_finder_cats_and_dogs import get_images
+from src.utils.corpus_creator import make_corpus
 
 
 SAVE_PATH=os.path.join(parentdir, "files")
@@ -49,5 +56,42 @@ def train_or_load_mnist(random_seed=42, cv=0, corpus_size=100, test_size=10, ran
     return classifier, (corpus_data, corpus_latents, corpus_target), (test_data, test_targets, test_latents)
 
 
-def train_or_load_new_dataset_model():
-    print("TODO: Lucas")
+def train_or_load_new_dataset_model(random_seed=42, cv=0, corpus_size=100, test_size=10, random_dataloader=False):
+    
+    torch.manual_seed(seed=random_seed)
+    
+    save_path = os.path.join(SAVE_PATH,f"model_cad{cv}.pth")
+    if not os.path.isfile(save_path):
+        train_model(save_path=save_path, random_seed=random_seed)
+    
+    classifier = load_model(save_path)
+    
+    test_dir = r"data\Animal Images\test"
+
+    picture_files, labels = get_images(test_dir)
+    
+    test_set = CandDDataSet(image_paths=picture_files, labels=labels)
+
+    test_loader = DataLoader(test_set, batch_size=200, shuffle=True)
+
+    (test_data, test_targets) = make_corpus(test_loader, corpus_size=10)
+
+    test_data = test_data.detach()
+    test_latents = classifier.latent_representation(test_data).detach()
+
+    corpus_dir = r"data\Animal Images\train"
+
+    picture_files = []
+
+    picture_files, labels = get_images(corpus_dir)
+    
+    corpus_set = CandDDataSet(image_paths=picture_files, labels=labels)
+
+    corpus_loader = DataLoader(corpus_set, batch_size=50, shuffle=True)
+
+    (corpus_data, corpus_target) = make_corpus(corpus_loader=corpus_loader)
+
+    corpus_data = corpus_data.detach()
+    corpus_latents = classifier.latent_representation(corpus_data).detach()
+
+    return classifier, (corpus_data, corpus_latents, corpus_target), (test_data, test_targets, test_latents)
