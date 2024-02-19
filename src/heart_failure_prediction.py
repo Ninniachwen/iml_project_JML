452b4d22b2f0ccbe36268f756e52a7e1000dc9ae
zainl <lukas.zain98@gmail.com>
2024-02-19 16:38:21 +0100

    training and classifier modifed and added

--- src/heart_failure_prediction.py
+++ src/heart_failure_prediction.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import DataLoader
 
-from src.Models.HeartfailureModel import HeartFailureClassifier
+from src.models.HeartfailureModel import HeartFailureClassifier
 from src.datasets.heartfailure_dataset import HeartFailureDataset     
 
 
@@ -26,7 +26,7 @@ def load_data(path):
     x = scaler.fit_transform(x)
     return x,y.values
 
-def train_model(model, save_path, x_train, y_train, x_test, y_test, device, epochs, lr=0.001):
+def train_heartfailure_model(model, save_path, x_train, y_train, x_test, y_test, random_seed=None, epochs=60, lr=0.001):
     """
     trains the model on the heartfailure predictions dataset
     :param model: NN for predictions
@@ -37,23 +37,22 @@ def train_model(model, save_path, x_train, y_train, x_test, y_test, device, epoc
     :param y_test: test target
 
     """
+    if random_seed:
+        torch.manual_seed(seed=random_seed)
 
     optimizer = torch.optim.Adam(params=model.parameters(),lr=lr)
 
     train_data = HeartFailureDataset(x_train,y_train)
     test_data = HeartFailureDataset(x_test,y_test)
     
-    train_loader = DataLoader(train_data, batch_size=50, shuffle=False)
-    test_loader = DataLoader(test_data, batch_size=50, shuffle=False)
+    train_loader = DataLoader(train_data, batch_size=50, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=50, shuffle=True)
     lossF = torch.nn.BCELoss()
 
     def train(epoch):
         model.train()
-        model.to(device)
         train_loss = 0
         for data, target in train_loader:
-            data = data.to(device)
-            target = target.to(device)
             optimizer.zero_grad()
             output = model(data)
             loss = lossF(output, target)
@@ -66,13 +65,10 @@ def train_model(model, save_path, x_train, y_train, x_test, y_test, device, epoc
 
     def test():
         model.eval()
-        model.to(device)
         test_loss = 0
         correct = 0
         with torch.no_grad():
             for data, target in test_loader:
-                data = data.to(device)
-                target = target.to(device)
                 output = model(data)
                 test_loss += lossF(output,target)
                 pred = (output >= 0.5).int()
@@ -86,20 +82,3 @@ def train_model(model, save_path, x_train, y_train, x_test, y_test, device, epoc
     test() 
     torch.save(model.state_dict(), save_path)
 
-
-def main():
-    datapath = "./data/heart.csv"
-    x,y = load_data(datapath)
-    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.1,random_state=42,shuffle=False)
-    model = HeartFailureClassifier()
-    epochs=40
-    if torch.cuda.is_available():
-        device=torch.device("cuda")
-    else:
-        device = torch.device("cpu")
-    train_model(model, save_path="./results/models/heart_failure_1.pth", x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,epochs=epochs, device=device)
-    
-
-
-if __name__ == "__main__":
-    main()
