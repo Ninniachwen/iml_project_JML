d602188fac47d0f937c83b2b9a3266beafc7ceff
zainl <lukas.zain98@gmail.com>
2024-02-20 20:29:54 +0100

    Dataset bug resolved

--- src/classifier_versions.py
+++ src/classifier_versions.py
@@ -15,9 +15,8 @@ sys.path.insert(0, "")  # noqa
 
 from original_code.src.simplexai.models.image_recognition import MnistClassifier
 from original_code.src.simplexai.experiments import mnist
-from src.cats_and_dogs_training import train_model
+from src.cats_and_dogs_training import train_model, load_model
 from src.models.CatsAndDogsModel import CatsandDogsClassifier
-from src.cats_and_dogs_predictions import load_model
 from src.datasets.cats_and_dogs_dataset import CandDDataSet
 from src.utils.image_finder_cats_and_dogs import get_images
 from src.utils.corpus_creator import make_corpus
@@ -48,11 +47,12 @@ def train_or_load_mnist(random_seed=42, cv=0, corpus_size=100, test_size=10, ran
     # data loader from approximate_quality
     corpus_loader = mnist.load_mnist(corpus_size, train=True, shuffle=random_dataloader)
     test_loader = mnist.load_mnist(test_size, train=False, shuffle=random_dataloader)
-    batch_id_test , (test_data, test_targets) = next(enumerate(test_loader))
+    (test_data, test_targets) = make_corpus(test_loader, corpus_size=test_size, n_classes=10)
     test_data = test_data.detach()
     test_latents = classifier.latent_representation(test_data).detach()
-    batch_id_corpus, (corpus_data, corpus_target) = next(enumerate(corpus_loader))
+    (corpus_data, corpus_target) = make_corpus(corpus_loader, corpus_size=corpus_size, n_classes=10)
     corpus_data = corpus_data.detach()
+    print(test_data.shape, test_targets)
     corpus_latents = classifier.latent_representation(corpus_data).detach()
     #TODO: maybe implement own mnist latents fuction?
     #TODO: which detach is truely necessary?
@@ -60,7 +60,7 @@ def train_or_load_mnist(random_seed=42, cv=0, corpus_size=100, test_size=10, ran
     return classifier, (corpus_data, corpus_target, corpus_latents), (test_data, test_targets, test_latents)
 
 
-def train_or_load_CaN_model(random_seed=42, cv=0, corpus_size=100, test_size=10, random_dataloader=False):
+def train_or_load_CaD_model(random_seed=42, cv=0, corpus_size=100, test_size=10, random_dataloader=False):
 
     torch.manual_seed(seed=random_seed)
     
@@ -72,17 +72,17 @@ def train_or_load_CaN_model(random_seed=42, cv=0, corpus_size=100, test_size=10,
     test_dir = r"data\Animal Images\test"
 
     picture_files, labels = get_images(test_dir)
-    test_set = CandDDataSet(image_paths=picture_files, labels=labels)
-    test_loader = DataLoader(test_set, batch_size=200, shuffle=True)
+    test_set = CandDDataSet(image_paths=picture_files, labels=torch.tensor(labels, dtype=torch.int64))
+    test_loader = DataLoader(test_set, batch_size=200, shuffle=random_dataloader)
     (test_data, test_targets) = make_corpus(test_loader, corpus_size=test_size)
     test_data = test_data.detach()
     test_latents = classifier.latent_representation(test_data).detach()
 
     corpus_dir = r"data\Animal Images\train"
-
+    
     picture_files, labels = get_images(corpus_dir)
-    corpus_set = CandDDataSet(image_paths=picture_files, labels=labels)
-    corpus_loader = DataLoader(corpus_set, batch_size=200, shuffle=True)
+    corpus_set = CandDDataSet(image_paths=picture_files, labels=torch.tensor(labels, dtype=torch.int64))
+    corpus_loader = DataLoader(corpus_set, batch_size=200, shuffle=random_dataloader)
     (corpus_data, corpus_target) = make_corpus(corpus_loader=corpus_loader, corpus_size=corpus_size)
     corpus_data = corpus_data.detach()
     corpus_latents = classifier.latent_representation(corpus_data).detach()
@@ -95,21 +95,19 @@ def train_or_load_heartfailure_model(random_seed=42, cv=0, corpus_size=100, test
 
     datapath = r"data\heart.csv"
     x,y = load_data(datapath)
-    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.1,random_state=42,shuffle=True)
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.1,random_state=42,shuffle=random_dataloader)
     classifier = HeartFailureClassifier()
     file_w_path = os.path.join(SAVE_PATH, f"model_heartfailure_{cv}.pth")
     if not os.path.isfile(file_w_path):
         train_heartfailure_model(classifier, save_path=file_w_path, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, cv=cv)
     classifier.load_state_dict(torch.load(file_w_path))
-
     train_data = HeartFailureDataset(x_train,y_train)
     test_data = HeartFailureDataset(x_test,y_test)
     
-    train_loader = DataLoader(train_data, batch_size=50, shuffle=True)
-    test_loader = DataLoader(test_data, batch_size=50, shuffle=True)
+    train_loader = DataLoader(train_data, batch_size=50, shuffle=random_dataloader)
+    test_loader = DataLoader(test_data, batch_size=50, shuffle=random_dataloader)
 
     (corpus_data, corpus_target) = make_corpus(train_loader, corpus_size=corpus_size)
-
     (test_data, test_targets) = make_corpus(test_loader, corpus_size=test_size)
 
     corpus_data = corpus_data.detach()
@@ -121,3 +119,7 @@ def train_or_load_heartfailure_model(random_seed=42, cv=0, corpus_size=100, test
     test_latents = classifier.latent_representation(test_data).detach()
 
     return classifier, (corpus_data, corpus_target, corpus_latents), (test_data, test_targets, test_latents)
+
+if __name__=="__main__":
+    train_or_load_heartfailure_model()
+    train_or_load_CaD_model()
--- src/utils/corpus_creator.py
+++ src/utils/corpus_creator.py
@@ -4,36 +4,40 @@ import random
 
 def make_corpus(corpus_loader: DataLoader, corpus_size=100, n_classes=2):
     """
-    incrementally samples from corpus loader, keeps class balance, performs reservoir sampling to sample with equal likelihood for every file
+    incrementally samples from corpus loader, keeps class balance, performs reservoir sampling to sample with equal likelihood for every file fore ach class in the loader.
     """
+
+    if corpus_size > len(corpus_loader.dataset): 
+        corpus_size = len(corpus_loader.dataset)
     corpus_files = []
     labels = []
     labelk = [0]*n_classes
 
     for data, label in corpus_loader:# do reservoir sampling
         for file, label in zip(data, label):
+            labelk[label]+=1
             if(len(corpus_files)>=corpus_size):# if corpus is full replace random element with probability (corpus_size/2)/labelk[label]
                 corpus_1_indices = [i for i,x in enumerate(labels) if x == label]
-                labelk[label]+=1
                 sample_index = random.choice(range(labelk[label]))
                 if sample_index < len(corpus_1_indices):
                     corpus_files[corpus_1_indices[sample_index]]=file
                     labels[corpus_1_indices[sample_index]]=label
+                    
             else:     # fill corpus with files, keep class balance
-                if labels.count(label)<corpus_size//n_classes:
+                if labels.count(label)<corpus_size+1//n_classes:
                     corpus_files.append(file)
                     labels.append(label)
-                    labelk[label]+=1
                 else:
                     corpus_1_indices = [i for i,x in enumerate(labels) if x == label]
-                    labelk[label]+=1
+                    
                     sample_index = random.choice(range(labelk[label]))
                     if sample_index < len(corpus_1_indices):
                         corpus_files[corpus_1_indices[sample_index]]=file
                         labels[corpus_1_indices[sample_index]]=label
-
+                        
     corpus_files = torch.stack(corpus_files)    
     labels = torch.stack(labels)
+    labels = labels.reshape(shape=(corpus_size,))
 
     return [corpus_files, labels]
 
