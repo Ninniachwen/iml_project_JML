160f45bf253fdeeeb0b8ac109309c368601e814c
zainl <lukas.zain98@gmail.com>
2024-02-20 21:48:42 +0100

    images working

--- src/visualization/images.py
+++ src/visualization/images.py
@@ -3,15 +3,14 @@ import matplotlib.gridspec as gridspec
 import itertools
 import torch
 import torchvision.transforms as t
+from captum.attr._utils.visualization import visualize_image_attr
 
-def plot_corpus_decomposition(test_image, corpus, weights, title: str = "") -> plt.Figure:
+def plot_corpus_decomposition(test_image, corpus, weights, decomposition_length=0, title: str = "") -> plt.Figure:
     """
-    Plots test image and corpus decomposition. Gives maximally 6 corpus images and jacobian coloration
+    Plots test image and corpus decomposition. Gives maximally 6 corpus images
     """
-    decomposition_length = len(weights)
-    if decomposition_length > 6: decomposition_length = 6
+    if decomposition_length < 1 or decomposition_length > 6 or not(type(decomposition_length)==int) or decomposition_length>len(weights): decomposition_length = 6
     sorted_weights_indices = torch.argsort(weights, descending=True)
-    print(weights.sum())
     sorted_weights = weights[sorted_weights_indices]
     sorted_images = corpus[sorted_weights_indices]
 
@@ -23,14 +22,13 @@ def plot_corpus_decomposition(test_image, corpus, weights, title: str = "") -> p
     ax.imshow(test_image.permute(1,2,0),cmap="grey")
     ax.axis('off')
     ax.set_title('Test Image')
-    print(sorted_weights.sum())
     grid_x_size = (decomposition_length-1)//2+1 
 
 
     gs2 = gridspec.GridSpecFromSubplotSpec(grid_x_size, 2, subplot_spec=gs[1])
     index = 0
     for y,x in itertools.product(range(2),range(grid_x_size)):
-        if index >=len(sorted_images): break
+        if index >=len(sorted_images) | index >decomposition_length: break
 
         ax = fig.add_subplot(gs2[x,y])
         ax.imshow(sorted_images[index].permute(1,2,0), cmap="grey")
@@ -42,12 +40,11 @@ def plot_corpus_decomposition(test_image, corpus, weights, title: str = "") -> p
     return fig
 
 
-def plot_corpus_decomposition_with_jacobian(test_image, corpus, weights, jacobian, title: str = "") -> plt.Figure:
+def plot_corpus_decomposition_with_jacobian(test_image, save_path, corpus, weights, jacobian, test_id, decomposition_length=0, title: str = "") -> plt.Figure:
     """
-    Plots test image and corpus decomposition. Gives maximally 5 corpus images and jacobian coloration
+    Plots test image and corpus decomposition. Gives maximally 6 corpus images and jacobian coloration
     """
-    decomposition_length = len(weights)#TODO: recompostionlength=1?
-    if decomposition_length > 6: decomposition_length = 6
+    if decomposition_length > 6 | decomposition_length>len(corpus): decomposition_length = 6
     sorted_weights_indices = torch.argsort(weights, descending=True)
     
     sorted_weights = weights[sorted_weights_indices]
@@ -55,34 +52,35 @@ def plot_corpus_decomposition_with_jacobian(test_image, corpus, weights, jacobia
     jacobian = jacobian[sorted_weights_indices]
     fig = plt.figure(layout='constrained')
 
-    gs = gridspec.GridSpec(1,2, figure=fig, width_ratios=[0.3,0.7])
+    gs = gridspec.GridSpec(1,2, figure=fig, width_ratios=[0.2,0.8])
 
     ax = fig.add_subplot(gs[0])
     ax.imshow(test_image.permute(1,2,0),cmap="grey")
     ax.axis('off')
     ax.set_title('Test Image')
-    
     grid_x_size = (decomposition_length-1)//2+1 
 
 
-    gs2 = gridspec.GridSpecFromSubplotSpec(grid_x_size, 2, subplot_spec=gs[1])
+    gs2 = gridspec.GridSpecFromSubplotSpec(grid_x_size, 2, subplot_spec=gs[1], hspace=0.2)
     index = 0
     for y,x in itertools.product(range(2),range(grid_x_size)):
-        if index >=len(sorted_images): break
+        if index >=decomposition_length: break
+        image = sorted_images[index].numpy().transpose(1,2,0)
+        saliency = jacobian[index].numpy().transpose(1,2,0)
+        title = f"Weight: {sorted_weights[index]*100:.2f}%"
         ax = fig.add_subplot(gs2[x,y])
-        ax.imshow(sorted_images[index].permute(1,2,0), cmap="grey")
-        #print(jacobian[index], jacobian[index].shape)
-        #print( "min",jacobian[index].min(1, keepdim=True)[0])
-        #jacobian[index] -= jacobian[index].min(1, keepdim=True)[0]
-        #print(jacobian[index])
-        #jacobian[index] /= jacobian[index].max(1, keepdim=True)[0]
-        #jacobian[index] = torch.nn.functional.log_softmax(jacobian[index], dim=0)
-        #print(jacobian[index])
-        ax.imshow(jacobian[index].permute(1,2,0),cmap='RdYlGn', alpha=1)
-        ax.axis('off')
-        ax.set_title(f"{sorted_weights[index].detach().item()*100:.2f}%")
+        visualize_image_attr(#TODO: finish
+            saliency,
+            image,
+            method="blended_heat_map",
+            sign="all",
+            plt_fig_axis=(fig, ax),
+            title=title,
+            alpha_overlay=0.6,
+            show_colorbar=True,
+            use_pyplot=False
+        )
         index += 1
     
-    
-    
     return fig
+
