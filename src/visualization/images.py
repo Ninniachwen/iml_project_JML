cbbc1fd739204b4d7fbb3ef6f22c894ec3be11ef
zainl <lukas.zain98@gmail.com>
2024-02-25 11:25:31 +0100

    images cleaned

--- src/visualization/images.py
+++ src/visualization/images.py
@@ -5,57 +5,6 @@ import torch
 from typing import List
 from captum.attr._utils.visualization import visualize_image_attr
 
-def plot_corpus_decomposition(test_image: torch.Tensor, test_pred: str, corpus: torch.Tensor, corpus_preds: List[str], weights: torch.Tensor, decomposition_length: int=4) -> plt.Figure:
-    """Creates a figure with the most important corpus examples and respective weight
-
-    Args:
-        test_image (torch.Tensor): test iamge for the corpus decomposition
-        test_pred (str): prediction for the test image
-        corpus (torch.Tensor): corpus images
-        corpus_preds (List[str]): predictions for the corpus images
-        weights (torch.Tensor): weights for the corpus images
-        decomposition_length (int, optional): how many corpus images shall be printed. Defaults to 4.
-
-    Returns:
-        plt.Figure: figure with test images and most important corpus examples
-    """
-    if decomposition_length < 1 or decomposition_length > 6 or not(type(decomposition_length)==int): decomposition_length = 6
-    if decomposition_length>len(weights): decomposition_length=len(weights)
-    sorted_weights_indices = torch.argsort(weights, descending=True)
-    sorted_weights = weights[sorted_weights_indices]
-    sorted_images = corpus[sorted_weights_indices]
-
-    fig = plt.figure(layout='constrained')
-
-    gs = gridspec.GridSpec(1,2, figure=fig, width_ratios=[0.3,0.7])
-
-    ax = fig.add_subplot(gs[0])
-    ax.imshow(test_image.permute(1,2,0),cmap="grey")
-    ax.axis('off')
-    ax.set_title(test_pred)
-    grid_x_size = (decomposition_length-1)//2+1 
-
-    if decomposition_length==1:
-        ax = fig.add_subplot(gs[1])
-        ax.imshow(sorted_images[0].permute(1,2,0), cmap="grey")
-        ax.axis('off')
-        ax.set_title(f"{corpus_preds[sorted_weights_indices[index]]}, Weigth:{sorted_weights[0].detach().item()*100:.2f}%")
-        return fig
-    else:
-        gs2 = gridspec.GridSpecFromSubplotSpec(grid_x_size, 2, subplot_spec=gs[1])
-        index = 0
-        
-        for y,x in itertools.product(range(2),range(grid_x_size)):
-            if  index >= decomposition_length: break
-            ax = fig.add_subplot(gs2[x,y])
-            ax.imshow(sorted_images[index].permute(1,2,0), cmap="grey")
-            ax.axis('off')
-            ax.set_title(f"{corpus_preds[sorted_weights_indices[index]]}, Weigth:{sorted_weights[index].detach().item()*100:.2f}%")
-            index += 1
-    
-    return fig
-
-
 def plot_corpus_decomposition_with_jacobian(test_image: torch.Tensor, test_pred: str, corpus: torch.Tensor, corpus_preds: List[str], weights: torch.Tensor, jacobian: torch.Tensor, decomposition_length: int=4) -> plt.Figure:
     """Creates a figure with the most important corpus examples and jacobian projection
 
